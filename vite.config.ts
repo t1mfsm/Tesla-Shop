2a091b5915eca09c1978869d79d7bcd615346a6b
t1mfsm <shakirov.timurshakirov@yandex.ru>
2024-12-22 22:18:04 +0300

    asf

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://192.168.0.152:8000',
+        target: 'http://192.168.114.153:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api')
       }
